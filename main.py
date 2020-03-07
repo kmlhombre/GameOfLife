3486dbe2f9a198d4c367c1e8506dc4f45bd15edd
kmlhombre <osak.k98@gmail.com>
2020-03-07 17:09:47 +0100

    App is ready to launch. Before testing

--- main.py
+++ main.py
@@ -13,14 +13,74 @@ def displayMap(map):
         print('  '.join(x))
 
 
-def initGame():
-    # init dead map
+def letsTheGameBegin(map):
+    while aliveCells > 0:
+        copy_map = initMap()
+        for x in range(height):
+            for y in range(width):
+                aliveNeighbours = 0
+                left = 0
+                right = width - 1
+                if map[x][y] == 'X':
+                    if y > 1:
+                        left = y-1
+                    if y < right - 2:
+                        right = y+1
+                    if x != 0:
+                        for z in range(left, right):
+                            if map[x-1][z] == 'O':
+                                aliveNeighbours += 1
+                    if map[x][left] == 'O':
+                        aliveNeighbours += 1
+                    if map[x][right] == 'O':
+                        aliveNeighbours += 1
+                    if x != height - 1:
+                        for z in range(left, right):
+                            if map[x+1][z] == 'O':
+                                aliveNeighbours += 1
+                    if aliveNeighbours == 3:
+                        copy_map[x][y] = 'O'
+                aliveNeighbours = 0
+                if map[x][y] == 'O':
+                    if y > 1:
+                        left = y-1
+                    if y < right - 1:
+                        right = y+1
+                    if x != 0:
+                        for z in range(left, right):
+                            if map[x - 1][z] == 'O':
+                                aliveNeighbours += 1
+                    if map[x][left] == 'O':
+                        aliveNeighbours += 1
+                    if map[x][right] == 'O':
+                        aliveNeighbours += 1
+                    if x != height - 1:
+                        for z in range(left, right):
+                            if map[x + 1][z] == 'O':
+                                aliveNeighbours += 1
+                    if aliveNeighbours == 2 or aliveNeighbours == 3:
+                        copy_map[x][y] = 'O'
+        map = copy_map
+        displayMap(map)
+        print('--------------------------')
+        control = input()
+        if control == 's':
+            break
+
+
+def initMap():
     map = []
     for _ in range(height):
         record = []
         for _ in range(width):
             record.append('X')
         map.append(record)
+    return map
+
+
+def initGame():
+    # init dead map
+    map = initMap()
 
     # add alive cells
     for _ in range(aliveCellsOnStart):
@@ -30,8 +90,11 @@ def initGame():
                 map[int(number/5)][number%5] = 'O'
                 break
 
-    # display test map
+    # display initial map
     displayMap(map)
+    print('--------------------------')
+
+    letsTheGameBegin(map)
 
 
 if __name__ == '__main__':
