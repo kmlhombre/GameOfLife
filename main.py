73fd7457b565b06fb557e42c9150e5bf971286a5
kmlhombre <osak.k98@gmail.com>
2020-03-08 11:19:50 +0100

    Code Optimalization. Bug fix #2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 # basic parameters of map
-aliveCellsOnStart = 4
+aliveCellsOnStart = 10
 height = 5
 width = 5
 
@@ -13,58 +13,47 @@ def displayMap(map):
         print('  '.join(x))
 
 
+def checkNeighbours(map, x, y):
+    aliveNeighbours = 0
+    left = 0
+    right = width - 1
+    if y > 1:
+        left = y - 1
+    if y < right - 2:
+        right = y + 1
+    if x != 0:
+        for z in range(left, right):
+            if map[x - 1][z] == 'O':
+                aliveNeighbours += 1
+    if map[x][left] == 'O':
+        aliveNeighbours += 1
+    if map[x][right] == 'O':
+        aliveNeighbours += 1
+    if x != height - 1:
+        for z in range(left, right):
+            if map[x + 1][z] == 'O':
+                aliveNeighbours += 1
+    return aliveNeighbours
+
+
 def letsTheGameBegin(map):
     global aliveCells
-    nextAliveCells = 0
     while aliveCells > 0:
+        nextAliveCells = 0
         copy_map = initMap()
         for x in range(height):
             for y in range(width):
-                aliveNeighbours = 0
-                left = 0
-                right = width - 1
+                aliveNeighbours = checkNeighbours(map, x, y)
                 if map[x][y] == 'X':
-                    if y > 1:
-                        left = y-1
-                    if y < right - 2:
-                        right = y+1
-                    if x != 0:
-                        for z in range(left, right):
-                            if map[x-1][z] == 'O':
-                                aliveNeighbours += 1
-                    if map[x][left] == 'O':
-                        aliveNeighbours += 1
-                    if map[x][right] == 'O':
-                        aliveNeighbours += 1
-                    if x != height - 1:
-                        for z in range(left, right):
-                            if map[x+1][z] == 'O':
-                                aliveNeighbours += 1
                     if aliveNeighbours == 3:
                         copy_map[x][y] = 'O'
                         nextAliveCells += 1
-                aliveNeighbours = 0
                 if map[x][y] == 'O':
-                    if y > 1:
-                        left = y-1
-                    if y < right - 1:
-                        right = y+1
-                    if x != 0:
-                        for z in range(left, right):
-                            if map[x - 1][z] == 'O':
-                                aliveNeighbours += 1
-                    if map[x][left] == 'O':
-                        aliveNeighbours += 1
-                    if map[x][right] == 'O':
-                        aliveNeighbours += 1
-                    if x != height - 1:
-                        for z in range(left, right):
-                            if map[x + 1][z] == 'O':
-                                aliveNeighbours += 1
-                    if aliveNeighbours == 2 or aliveNeighbours == 3:
+                    if aliveNeighbours == 3 or aliveNeighbours == 2:
                         copy_map[x][y] = 'O'
                         nextAliveCells += 1
         aliveCells = nextAliveCells
+        print(aliveCells)
         map = copy_map
         displayMap(map)
         print('--------------------------')
@@ -88,17 +77,12 @@ def initGame():
     map = initMap()
 
     # add alive cells
-    # for _ in range(aliveCellsOnStart):
-    #   while True:
-    #        number = randint(0, 24)
-    #        if map[int(number/5)][number%5] == 'X':
-    #            map[int(number/5)][number%5] = 'O'
-    #            break
-
-    map[2][3] = 'O'
-    map[2][4] = 'O'
-    map[3][2] = 'O'
-    map[3][3] = 'O'
+    for _ in range(aliveCellsOnStart):
+        while True:
+            number = randint(0, 24)
+            if map[int(number/5)][number%5] == 'X':
+                map[int(number/5)][number%5] = 'O'
+                break
 
     # display initial map
     displayMap(map)
